5501b7861d52304641773ce8261b79adb177ab42
Deepak Kumar <vt931515@gmail.com>
2026-06-12 22:04:06 +0530

    Added new programs

---
+++ StackAndQueue/trappingRainWatter.java
@@ -0,0 +1,30 @@
+// trapping rain water
+public class Main {
+
+    public static void main(String[] args) {
+
+        int arr[] = {3, 0, 0, 2, 0, 4};
+        int water = 0;
+
+        for (int i = 1; i < arr.length - 1; i++) {
+
+            int leftMax = arr[i];
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > leftMax)
+                    leftMax = arr[j];
+            }
+
+            int rightMax = arr[i];
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > rightMax)
+                    rightMax = arr[j];
+            }
+
+            water += Math.min(leftMax, rightMax) - arr[i];
+        }
+
+        System.out.println("Water Trapped = " + water);
+    }
+}
