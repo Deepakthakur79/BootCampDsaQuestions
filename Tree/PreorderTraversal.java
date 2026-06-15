16cb989846fae8dcf2794a59f262fe6a247cb17d
Deepak Kumar <vt931515@gmail.com>
2026-06-15 21:41:58 +0530

    Added new programs

---
+++ Tree/PreorderTraversal.java
@@ -0,0 +1,19 @@
+// Preorder Traversal using Array Representation .
+import java.util.Scanner;
+public class PreorderTraversal {
+    static void preorder(int tree[], int i) {
+         if (i >= tree.length)
+             return;
+
+        System.out.print(tree[i] + " ");
+        preorder(tree, 2 * i + 1);
+        preorder(tree, 2 * i + 2);
+    }
+
+    public static void main(String[] args) {
+        int tree[] = {1, 2, 3, 4, 5, 6, 7};
+
+        System.out.print("Preorder: ");
+        preorder(tree, 0);
+    }
+}
