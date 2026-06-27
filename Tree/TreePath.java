b92c0f21be413f445ddd61ddcc60d7e8c624d010
Deepak Kumar <vt931515@gmail.com>
2026-06-27 22:36:47 +0530

    Added new programs

---
+++ Tree/TreePath.java
@@ -0,0 +1,53 @@
+//binary tree paths
+
+import java.util.*;
+
+class BinaryTreePaths {
+
+    static class Node {
+
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+
+    static void printPaths(Node root, String path) {
+
+        if(root == null)
+            return;
+
+
+        path = path + root.data;
+
+
+        // leaf node
+        if(root.left == null && root.right == null) {
+            System.out.println(path);
+            return;
+        }
+
+
+        printPaths(root.left, path + "->");
+
+        printPaths(root.right, path + "->");
+    }
+
+
+    public static void main(String[] args) {
+
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.right = new Node(5);
+
+
+        printPaths(root, "");
+    }
+}
