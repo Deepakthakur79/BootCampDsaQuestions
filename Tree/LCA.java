ca68e68ad31c1b685b76d50f94cf995ef6c318c6
Deepak Kumar <vt931515@gmail.com>
2026-07-01 22:29:29 +0530

    Added new programs

---
+++ Tree/LCA.java
@@ -0,0 +1,31 @@
+// lowest common ancestor of two nodes in a binary tree
+import java.util.*;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root,TreeNode p,TreeNode q) {
+
+        if(root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+
+        if(left != null && right != null) {
+            return root;
+        }
+
+        return left != null ? left : right;
+    }
+}
